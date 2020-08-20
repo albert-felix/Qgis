65f77d0a3f032847dfc6f7bbac5a88837d9a0b89
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-08-20 15:44:48 +0530

    Update check_branch_test.py

--- check_branch_test.py
+++ check_branch_test.py
@@ -6,19 +6,20 @@ def goto():
     count = 0
     while (count <=10):
         keyboard.press_and_release('ctrl+c')
-        time.sleep(1)
+        time.sleep(0.2)
         keyboard.press_and_release('alt+tab')
-        time.sleep(1)
+        time.sleep(0.2)
         keyboard.press_and_release('ctrl+g')
-        time.sleep(1)
+        time.sleep(0.3)
         keyboard.press_and_release('ctrl+v')
-        time.sleep(1)
+        time.sleep(0.2)
         keyboard.press_and_release('enter')
+        #keyboard.wait('esc')
         time.sleep(3)
         keyboard.press_and_release('alt+tab')
-        time.sleep(1)
+        time.sleep(0.2)
         keyboard.press_and_release('down')
-        time.sleep(1)
+        time.sleep(0.2)
         count+=1
 
 a = Timer(8.0, goto)
